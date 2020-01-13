47246e1df83ac9623f4ab0f6d49600bfb514af57
Vasilii Gavshin <vasilii_gavshin@epam.com>
2020-01-13 17:50:22 +0300

    Added dist to repository

---
+++ lib/dist/interfaces/Events.d.ts
@@ -0,0 +1,13 @@
+export interface CommandCompletedEvent {
+    commandName: string;
+    commandId: string;
+    completionTime: number;
+    params: any;
+}
+export interface BottomTabSelectedEvent {
+    selectedTabIndex: number;
+    unselectedTabIndex: number;
+}
+export interface BottomTabLongPressedEvent {
+    selectedTabIndex: number;
+}
